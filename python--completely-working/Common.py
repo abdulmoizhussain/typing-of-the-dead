7f015982f7d831b33721027a688716e84ab59ec5
unknown <moizhus123@gmail.com>
2019-09-09 20:52:19 +0500

    --code format

--- python--completely-working/Common.py
+++ python--completely-working/Common.py
@@ -14,6 +14,7 @@ window_processed = "Processed"
 def avg_of_difference(a, b, percent):
     return (a if a < b else b) + (abs(a - b) * percent)
 
+
 def put_text(img, text, x, y):
     cv.putText(img,
                text,
